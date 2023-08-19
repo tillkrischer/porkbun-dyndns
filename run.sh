da159109ca96b80bdf0d58721171a0574491ea61
Till Krischer <till@krischer.io>
2023-08-19 18:21:48 +0200

    dont append

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ command="node /update-dns.js"
 pipe=">/proc/1/fd/1 2>/proc/1/fd/2"
 cronstatement="${cronschedule} ${variableinit} ${command} ${pipe}"
 
-echo "$cronstatement" >> /etc/cron.d/update-dns
+echo "$cronstatement" > /etc/cron.d/update-dns
 crontab /etc/cron.d/update-dns
 cron -f
